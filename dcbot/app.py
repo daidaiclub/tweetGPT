9fdb02741f930462c45822a941d4c1f7a4628cd7
jimmyhealer <yahing6066@gmail.com>
2023-06-24 12:20:39 +0800

    init commit

---
+++ dcbot/app.py
@@ -0,0 +1,57 @@
+import discord
+import asyncio
+import aiohttp
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path='./.env.local')
+
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')  # 你的 bot token
+backend_url = os.getenv('BACKEND_URL') # 你的後端 url
+
+class MyBot(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+        self.session = aiohttp.ClientSession()
+        self.bg_task = self.loop.create_task(self.background_task())
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('!set'):
+            # 擷取訊息
+            msg = message.content[len('!set'):].strip()
+            print()
+            # 傳送資訊到後端
+            async with self.session.post(backend_url, json={'brand': msg, 'channel_id': message.channel.channel_id}) as response:
+                if response.status == 200:
+                    await message.channel.send('已紀錄品牌名稱，當有新的策略時會通知您。')
+                else:
+                    await message.channel.send('紀錄品牌名稱失敗，請稍後再試。')
+
+    async def background_task(self):
+        await self.wait_until_ready()
+        while not self.is_closed():
+            print('Checking new data...')
+            async with self.session.get(backend_url) as response:
+                data = await response.json()
+                print(response.status, data)
+                if response.status == 200 and data:
+                    for item in data:
+                        channel = self.get_channel(item['channel_id'])  # 你的頻道 ID
+                        await channel.send(item['message'])
+            await asyncio.sleep(500)  # 等待 5 秒
+
+    async def close(self):
+        await self.session.close()
+        await super().close()
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+client = MyBot(intents=intents)
+client.run(TOKEN)
---
+++ flask/app.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from rq import Queue
+from rq.job import Job
+from worker import conn
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'  # 將這個路徑改為你的 sqlite3 資料庫的路徑
+db = SQLAlchemy(app)
+
+q = Queue(connection=conn)
+
+# 創建一個數據模型
+class Brand(db.Model):
+  channel_id = db.Column(db.Integer(), primary_key=True)
+  brand = db.Column(db.String(120), nullable=False)
+
+class Result(db.Model):
+  channel_id = db.Column(db.Integer(), primary_key=True)
+  result = db.Column(db.String(1200), nullable=False)
+
+def get_tweets(brand, channel_id):
+  # TODO: 使用品牌名稱調用 Twitter API 獲取 tweet 數據
+  # 儲存或處理數據的邏輯實現，在這裡寫好 prompt
+  return {'prompt': '這是一個 prompt', 'channel_id': channel_id}
+
+@app.route('/brand', methods=['POST'])
+def brand():
+  # 保存channel_id 和 品牌名稱到資料庫
+  data = request.get_json()
+
+  # 使用品牌名稱調用 Twitter API 獲取 tweet 數據，並將它加入工作佇列
+  job = q.enqueue_call(
+    func=get_tweets, args=(data['brand'], data['channel_id']), result_ttl=5000
+  )
+  
+  return {'job_id': job.get_id()}, 202
+
+@app.route('/queue', methods=['GET'])
+def queue():
+  # 獲取工作佇列的內容
+  jobs = q.get_jobs()
+  results = [Job.fetch(job.get_id(), connection=conn).result for job in jobs]
+  
+  # 清空佇列
+  for job in jobs:
+    job.delete()
+  
+  return jsonify(results)
+
+@app.route('/gpt', methods=['POST'])
+def gpt():
+  # 紀錄 GPT 分析完的數據
+  data = request.get_json()
+  result = Result(channel_id=data['channel_id'], result=data['result'])
+  db.session.add(result)
+  db.session.commit()
+
+  return {'message': 'Data saved successfully'}, 200
+
+@app.route('/results', methods=['GET'])
+def results():
+  # 獲取 GPT 分析完的數據
+  results = Result.query.all()
+  data = [{'channel_id': result.channel_id, 'result': result.result} for result in results]
+
+  # 清空資料庫
+  for result in results:
+    db.session.delete(result)
+  return jsonify(data)
+
+if __name__ == '__main__':
+  db.create_all()
+  app.run(debug=True)
